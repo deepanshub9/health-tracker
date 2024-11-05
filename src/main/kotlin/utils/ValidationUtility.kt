b75d724930fdb260697a0a20375282b9b3b46bec
Deepanshu <kukukumar1996@gmail.com>
2024-11-05 10:58:52 +0000

    update file

--- src/main/kotlin/utils/ValidationUtility.kt
+++ src/main/kotlin/utils/ValidationUtility.kt
@@ -3,23 +3,23 @@ package utils
 class ValidationUtility {
 
 
-    #for_Gender
+    //for_Gender
     fun isValidGender(gender: Char): Boolean {
         return gender == 'F' || gender == 'M' || gender == 'O'
     }
 
-    #for_Email
+    //for_Email
     fun isValidEmail(email: String): Boolean {
         val emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$".toRegex()
         return email.matches(emailPattern)
     }
 
-    #for_Weight
+    //for_Weight
     fun isValidWeight(weight: Double): Boolean {
         return weight in 0.0..300.0
     }
 
-    #for_Height
+    //for_Height
     fun isValidHeight(height: Float): Boolean {
         return height in 0.5f..2.5f
     }
